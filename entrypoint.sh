44c1fcc6fdd2e69fc7c95f3119c52f358e650b0a
brian pursell <brian.pursell@raise.com>
2020-05-28 13:51:31 -0500

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ simple_json="$INPUT_SIMPLE_JSON"
 
 echo -e "before\n"
 #aws ssm get-parameter --name $parameter_name --output json >> output.txt
-aws sts get-caller-identity --output json >> output.txt
+aws sts get-caller-identity --output json --debug >> output.txt
 cat output.txt
 ssm_param='something else'
 echo -e "after\n"
